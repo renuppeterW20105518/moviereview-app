3e9c7572552a3b0ae91738a78b9d52af88b2eaac
Renu P Peter <20105518@mail.wit.ie>
2024-03-16 10:55:17 +0000

    Simple review standalone lambda

--- lib/moviereview-app-stack.ts
+++ lib/moviereview-app-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MoviereviewAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const reviewFn = new lambdanode.NodejsFunction(this, "ReviewFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/review.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MoviereviewAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
   }
 }
