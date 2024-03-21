771c73a4ce06df0fcbee7508645b189d9ff48f32
Renu P Peter <20105518@mail.wit.ie>
2024-03-21 22:53:43 +0000

    Added App API endpoints

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -55,6 +55,164 @@ export class AuthAppStack extends cdk.Stack {
     this.addAuthRoute('signout', 'GET', 'SignoutFn', 'signout.ts');
     this.addAuthRoute('signin', 'POST', 'SigninFn', 'signin.ts');
 
+    // NEW
+    const appApi = new apig.RestApi(this, "AppApi", {
+      description: "App RestApi",
+      endpointTypes: [apig.EndpointType.REGIONAL],
+      defaultCorsPreflightOptions: {
+        allowOrigins: apig.Cors.ALL_ORIGINS,
+      },
+    });
+
+    const appCommonFnProps = {
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "handler",
+      environment: {
+        USER_POOL_ID: this.userPoolId,
+        CLIENT_ID: this.userPoolClientId,
+        REGION: cdk.Aws.REGION,
+      },
+    };
+
+    const moviesEndpoint = appApi.root.addResource("movies");
+    const movieIdEndpoint = moviesEndpoint.addResource("{movieId}");
+    const reviewsContent = movieIdEndpoint.addResource("reviews");
+    const reviewerEndpoint = reviewsContent.addResource("{reviewerName}")
+    const addmovieReviewEndpoint = moviesEndpoint.addResource("reviews")
+
+    const reviewEndpoint = appApi.root.addResource("reviews");
+    const reviewreviewerNameEndpoint = reviewEndpoint.addResource("{reviewerName}");
+
+    //GET /reviews/{reviewerName}/{movieId}/translation?language=code
+    const langMovieIdEndpoint = reviewreviewerNameEndpoint.addResource("{movieId}").addResource("translation")
+   
+    const getMovieReviewByIdFn = new node.NodejsFunction(
+      this,
+      "GetMovieReviewByIdFn",
+      {
+        ...appCommonFnProps,
+        entry: `${__dirname}/../lambdas/getMovieReviewById.ts`,
+      }
+    );
+
+    const newMovieReviewFn = new node.NodejsFunction
+    (this, "AddMovieReviewFn", 
+    {
+      ...appCommonFnProps,
+      entry: `${__dirname}/../lambdas/postMovieReview.ts`,
+    });
+
+    const getMovieReviewByRatingFn = new node.NodejsFunction(
+      this,
+      "GetMovieReviewByRatingFn",
+      {
+        ...appCommonFnProps,
+        entry: `${__dirname}/../lambdas/getMovieReviewByRating.ts`,
+      }
+    );
+
+    const getMovieReviewByReviewerFn = new node.NodejsFunction(
+      this,
+      "GetMovieReviewByReviewerFn",
+      {
+        ...appCommonFnProps,
+        entry: `${__dirname}/../lambdas/getMovieReviewByReviewer.ts`,
+      }
+    );
+
+    const putMovieReviewByReviewerFn = new node.NodejsFunction(
+      this,
+      "PutMovieReviewByReviewerFn",
+      {
+        ...appCommonFnProps,
+        entry: `${__dirname}/../lambdas/putMovieReviewByReviewerFn.ts`,
+      }
+    );
+
+    const getReviewReviewerNameFn = new node.NodejsFunction(
+      this,
+      "GetReviewReviewerNameFn",
+      {
+        ...appCommonFnProps,
+        entry: `${__dirname}/../lambdas/getReviewReviewerName.ts`,
+      }
+    );
+
+    const languageFn = new node.NodejsFunction(
+      this,
+      "LanguageFn",
+      {
+        ...appCommonFnProps,
+        entry: `${__dirname}/../lambdas/languagetranslation.ts`,
+      }
+    );
+
+
+    const authorizerFn = new node.NodejsFunction(this, "AuthorizerFn", {
+      ...appCommonFnProps,
+      entry: `${__dirname}/../lambdas/auth/authorizer.ts`,
+    });
+
+    const requestAuthorizer = new apig.RequestAuthorizer(
+      this,
+      "RequestAuthorizer",
+      {
+        identitySources: [apig.IdentitySource.header("cookie")],
+        handler: authorizerFn,
+        resultsCacheTtl: cdk.Duration.minutes(0),
+      }
+    );
+
+    //GET /movies/{movieId}/reviews
+     reviewsContent.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getMovieReviewByIdFn, { proxy: true })
+    );
+
+    //POST /movies/reviews
+    addmovieReviewEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieReviewFn, { proxy: true }),{
+        authorizer: requestAuthorizer,
+        authorizationType: apig.AuthorizationType.CUSTOM,
+      });
+
+    //GET /movies/{movieId}/reviews/{reviewerName}
+    reviewerEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getMovieReviewByReviewerFn, { proxy: true })
+    );
+
+    //PUT /movies/{movieId}/reviews/{reviewerName}
+    reviewerEndpoint.addMethod(
+      "PUT",
+      new apig.LambdaIntegration(putMovieReviewByReviewerFn, { proxy: true }),{
+        authorizer: requestAuthorizer,
+        authorizationType: apig.AuthorizationType.CUSTOM,
+      });
+
+    //GET /reviews/{reviewerName}
+    reviewreviewerNameEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getReviewReviewerNameFn, { proxy: true })
+    );
+
+    langMovieIdEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(languageFn, { proxy: true })
+    );
+
+
+
+
+
+
+
+
+
   }
 
   private addAuthRoute(
